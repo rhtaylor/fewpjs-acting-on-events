8ff2f6ce0a399b202449582a8ec03ecb9e4505df
Randy <randyisbusy@gmail.com>
2020-02-07 10:07:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Your code here
+// Your code here 
+let dodger = document.getElementById("dodger"); 
+ 
+dodger.style.backgroundColor = "#FF69B4"; 
+let x = dodger.style.left; 
+let y = dodger.style.bottom;  
+let z = dodger.style.right;
+ 
+console.log(x, y, z); 
+
+dodger.style.bottom = "0px"; 
+let a = dodger.style.left; 
+let b = dodger.style.bottom;
+let c = dodger.style.right; 
+
+console.log(a, b, c,) 
+
+
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+debugger
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`; 
+        console.log(dodger, dodger.style)
+    }
+} 
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft(); 
+        console.log(e, e.key)
+    } 
+    else if (e.key === "ArrowRight") { 
+        moveDodgerRight();
+        console.log(e, e.key, z)
+        }  
+}); 
+function moveDodgerRight() { 
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10); 
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+        console.log(dodger, dodger.style)
+    }
+} 
